c537d85b8d898cff1fcab2c4fae6bdd90609a6f2
ghulevishal <vghule9994@gmail.com>
2020-08-26 08:48:21 +0530

    new update Pod

--- pods/add_testcases.sh
+++ pods/add_testcases.sh
@@ -1,16 +1,28 @@
-#!/bin/bash 
+#!/bin/bash
 
-cat > /tmp/pods.bats << EOF 
+cat > /tmp/pods.bats << EOF
 
 #!/usr/bin/env bats
 
 @test "create pod vote" {
-  result="\$(kubectl get pod vote > /dev/null 2>&1 && echo done)"
+  result="\$(kubectl get pod vote -n default > /dev/null 2>&1 && echo done)"
   [ "\$result" == "done" ]
 }
 
-@test "create pod dc" {
-  result="\$(kubectl get pod db > /dev/null 2>&1 && echo done)"
+@test "troubleshoot pod " {
+  result="\$(kubectl get pod nginx -n default > /dev/null 2>&1 && echo done)"
   [ "\$result" == "done" ]
 }
+
+@test "static pod " {
+  result="\$(kubectl get pod static-pod-controlplane -n default > /dev/null 2>&1 && echo done)"
+  [ "\$result" == "done" ]
+}
+
+@test "create pod in ns" {
+  result="\$(kubectl get pod mypod -n mynamespace > /dev/null 2>&1 && echo done)"
+  [ "\$result" == "done" ]
+}
+
+
 EOF
