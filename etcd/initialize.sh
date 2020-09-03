a46877a363e6157689ef1d37437f0af159b0fb90
ghulevishal <vghule9994@gmail.com>
2020-09-03 07:53:49 +0530

    etcd

--- etcd/initialize.sh
+++ etcd/initialize.sh
@@ -1,2 +1,7 @@
 launch.sh
 apt-get install -yq bats bc
+
+wget https://github.com/coreos/etcd/releases/download/v3.3.0-rc.1/etcd-v3.3.0-rc.1-linux-amd64.tar.gz
+tar -xvzf etcd-v3.3.0-rc.1-linux-amd64.tar.gz
+mv etcd-v3.3.0-rc.1-linux-amd64/etcd /usr/bin/.
+mv etcd-v3.3.0-rc.1-linux-amd64/etcdctl /usr/bin/.
