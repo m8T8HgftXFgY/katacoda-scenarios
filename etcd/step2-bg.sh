e64b7d343c76ceb63f8d8c2a499b352f7a340d9a
ghulevishal <vghule9994@gmail.com>
2020-09-03 08:26:32 +0530

    etcd

--- etcd/step2-bg.sh
+++ etcd/step2-bg.sh
@@ -1,8 +1,2 @@
-wget https://github.com/coreos/etcd/releases/download/v3.3.0-rc.1/etcd-v3.3.0-rc.1-linux-amd64.tar.gz
-tar -xvzf etcd-v3.3.0-rc.1-linux-amd64.tar.gz
-mv etcd-v3.3.0-rc.1-linux-amd64/etcd /usr/bin/.
-mv etcd-v3.3.0-rc.1-linux-amd64/etcdctl /usr/bin/.
-
-
 export ETCDCTL_API=3
 ETCDCTL_API=3 etcdctl --endpoints=https://127.0.0.1:2379 --cacert=/etc/kubernetes/pki/etcd/ca.crt --cert=/etc/kubernetes/pki/apiserver-etcd-client.crt --key=/etc/kubernetes/pki/apiserver-etcd-client.key snapshot save /tmp/cka-snapshotdb
